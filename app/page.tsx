0645258f2cf1780a4f734d6fdcf2a62cc0d6c3ac
nicochan1 <nico.chan@yahoo.com>
2026-02-01 11:31:41 +0800

    add http

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { EventCard } from "@/components/EventCard"
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 
 const page = async() => {
-  const response = await fetch(`${BASE_URL}/api/events`)
+  const response = await fetch(`https://${BASE_URL}/api/events`)
   const {events} = await response.json()
 
   
